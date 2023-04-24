5c05bfa36e8d676affefc6c9123836d843fb1503
Caleb Farrell <caleb.farrell987@gmail.com>
2023-04-24 15:47:41 -0300

    Moved scraper.py and reformatted

--- webscraper/main/scraper.py
+++ webscraper/main/scraper.py
@@ -1,3 +1,5 @@
+import os
+
 from bs4 import BeautifulSoup as bs
 import requests
 import pandas as pd
@@ -5,11 +7,12 @@ import urllib.parse
 
 page_size = 96
 page_num = 1
-start_url = 'https://www.newegg.ca/Internal-SSDs/SubCategory/ID-636/Page-' + str(page_num) + '?Tid=11700&PageSize=' + str(
+start_url = 'https://www.newegg.ca/Internal-SSDs/SubCategory/ID-636/Page-' + str(
+    page_num) + '?Tid=11700&PageSize=' + str(
     page_size)
 
-def get_pages(page_num, start_url):
 
+def get_pages(page_num, start_url):
     total_pages = 0
 
     if page_num == 1:
@@ -18,12 +21,13 @@ def get_pages(page_num, start_url):
             soup = bs(response.content, 'html.parser')
             results = soup.find_all('div', class_='list-tools-bar')
             pages = results[0].find('span', class_='list-tool-pagination-text').get_text()
-            pages = pages.replace('Page','')
+            pages = pages.replace('Page', '')
             current_page = pages.split('/')[0]
             total_pages = pages.split('/')[1]
             page_num = int(current_page)
             total_pages = int(total_pages)
     return page_num, total_pages
+
 
 def get_all_item_details_per_page(start_url):
     url = start_url
@@ -34,8 +38,6 @@ def get_all_item_details_per_page(start_url):
     if response.status_code == 200:
         soup = bs(response.content, 'html.parser')
         results = soup.find_all('div', class_='item-cell')
-
-
 
         for soup in results:
             try:
@@ -48,19 +50,19 @@ def get_all_item_details_per_page(start_url):
                 item_price.append('None')
 
     else:
-        print('Error: '+str(response.status_code))
+        print('Error: ' + str(response.status_code))
 
     return item_name, item_price
 
 
 def get_all_pages(page_size, page_num, total_pages, start_url):
-
     item_name = []
     item_price = []
 
     for page_num in range(page_num, total_pages):
         page_num += 1
-        start_url = 'https://www.newegg.ca/Internal-SSDs/SubCategory/ID-636/Page-' + str(page_num) + '?Tid=11700&PageSize=' + str(page_size)
+        start_url = 'https://www.newegg.ca/Internal-SSDs/SubCategory/ID-636/Page-' + str(
+            page_num) + '?Tid=11700&PageSize=' + str(page_size)
         print('Page: ' + str(page_num))
         name, price = get_all_item_details_per_page(start_url)
         item_name.extend(name)
@@ -77,7 +79,7 @@ def format_items(item_name, item_price):
     return items
 
 
-page_num,total_pages = get_pages(page_num, start_url)
+page_num, total_pages = get_pages(page_num, start_url)
 get_items = get_all_pages(page_size, page_num, total_pages, start_url)
 items = format_items(get_items[0], get_items[1])
 print(items)
